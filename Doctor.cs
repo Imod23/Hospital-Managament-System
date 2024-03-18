c4a329e4b78b61b38fc4f4b8f2f9a5297fd21e3c
it lec <it_lec@gmai,.com>
2024-03-18 11:14:46 +0530

    created doctor class

---
+++ Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Doctor
+
+{
+    public string Doctor name  { get; set; } 
+
+    public string Doc_Emp_no { get; set; }
+
+    public string Doc_Position { get; set; }
+
+    public int Arriving Time { get; set; }
+
+    public int Avialble date { get; set; }
+
+
+    public Doctor()
+	{
+	 
+    }
+}
+
+
